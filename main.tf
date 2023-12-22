fc4c22ac2908d4af3c637d7902bb208a472230a0
bworks84 <bworks84@gmail.com>
2023-12-22 22:31:34 +0000

    #11 Install TF provider, create S3 bucket

--- main.tf
+++ main.tf
@@ -7,15 +7,19 @@ terraform {
   }
 }
 
-  provider "random" {
-
+  provider "aws" {
   }
 
   resource "random_string" "bucket_name" {
-    length              = 16
+    lower               = true
+    upper               = false
+    length              = 32
     special             = false
-
   }
+
+  resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
+}
 
    output "random_bucket_name_result" {
     value = random_string.bucket_name.result
