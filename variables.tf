871485798a4e24701cbda10accc15ec87b881508
bworks84 <64917010+bworks84@users.noreply.github.com>
2024-01-25 16:16:56 -0500

    # 26 enabled static website hosting, uploaded index and error file via terraform (#27)

--- variables.tf
+++ variables.tf
@@ -5,3 +5,15 @@ variable "user_uuid" {
 variable "bucket_name" {
   type = string
 }
+
+variable "index_html_filepath" {
+  description = "Path to the index HTML file"
+  type        = string
+
+}
+
+variable "error_html_filepath" {
+  description = "Path to the error HTML file"
+  type        = string
+
+}
