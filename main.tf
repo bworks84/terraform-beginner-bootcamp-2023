772d5b1d3f4ffa60df8612fe0ed5650b04eceba2
bworks84 <bworks84@gmail.com>
2024-02-09 22:12:34 +0000

    #38 Implement TF provider block

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
   # cloud {
   #   organization = "Robs-Org-practice"
 
@@ -7,13 +13,19 @@ terraform {
   #   }
   # }
 }
- 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
+
+provider "terratowns" {
+  endpoint = "localhost:4567"
+  user_uuid = "fe2db730-0904-4ef3-b0ae-e4f2307af750"
+  token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
+ 
+# module "terrahouse_aws" {
+#   source = "./modules/terrahouse_aws"
+#   user_uuid = var.user_uuid
+#   bucket_name = var.bucket_name
+#   index_html_filepath = var.index_html_filepath
+#   error_html_filepath = var.error_html_filepath
+#   content_version = var.content_version
+#   assets_path = var.assets_path
+# }
