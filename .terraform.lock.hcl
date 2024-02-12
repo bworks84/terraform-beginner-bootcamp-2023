d481a9924239120971c6bd0936efa984d581e90e
bworks84 <64917010+bworks84@users.noreply.github.com>
2024-02-12 12:45:32 -0500

    38 terratowns provider (#44)
    * #38 Work in progress setting up skeleton for creating terraform provider
    
    * #38 Implement TF provider block
    
    * Update .gitpod.yml bug
    
    * Update .gitpod.yml
    
    * #38 Set up skeleton for resource
    
    * #38 Finished implementation of TF provider

---
+++ .terraform.lock.hcl
@@ -0,0 +1,10 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "local.providers/local/terratowns" {
+  version     = "1.0.0"
+  constraints = "1.0.0"
+  hashes = [
+    "h1:riGee29YVzU564WbjYgUXQmwm38R/TySp/6CZLU2eLE=",
+  ]
+}
--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid = "fe2db730-0904-4ef3-b0ae-e4f2307af750"
   token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -29,3 +29,14 @@ provider "terratowns" {
 #   content_version = var.content_version
 #   assets_path = var.assets_path
 # }
+
+resource "terratowns_home" "home" {
+  name = "Rally Point Coffee Shop"
+  description = <<DESCRIPTION
+This coffee shop serves as a rally point for all recondos
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
